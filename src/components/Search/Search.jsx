7e29af9af0ac970ab5ee6a5e887872facaa3f406
AJ Williams <ajwilliams.contact@gmail.com>
2022-01-19 09:56:06 +0200

    Refactoring code

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,16 @@
 import { Logo, SearchTitle, SearchBox } from './SearchStyle';
 
 const Search = ({ updateInput, getWeather }) => {
-    return (
-        <>
-            <Logo src='assets/day.svg' alt='Day' />
-            <SearchTitle>Enter City Name:</SearchTitle>
-            <SearchBox onSubmit={getWeather}>
-                <input type='text' placeholder='Cape Town' onChange={(e) => updateInput(e.target.value)} />
-                <button type='submit'>Search</button>
-            </SearchBox>
-        </>
-    );
+  return (
+    <>
+      <Logo src='assets/day.svg' alt='Day' />
+      <SearchTitle>Enter City Name:</SearchTitle>
+      <SearchBox onSubmit={getWeather}>
+        <input type='text' placeholder='Cape Town' onChange={(e) => updateInput(e.target.value)} />
+        <button type='submit'>Search</button>
+      </SearchBox>
+    </>
+  );
 };
 
 export default Search;
